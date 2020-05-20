fc1100b4c697d5f28985a55b59f5ba77fa032e35
Eduard <terriblegoodday@icloud.com>
2020-05-20 12:51:26 +1000

    Initial Commit

---
+++ Strings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Strings
+//
+//  Created by Eduard Dzhumagaliev on 5/20/20.
+//  Copyright © 2020 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
